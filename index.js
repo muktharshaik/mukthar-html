0dafe2c72ebd111fd18d771666c0f2fbc9b6526c
Mukthar <msshaikmukthar9@gmail.com>
2022-07-27 08:58:24 +0400

    billing address container

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+var createAccount = document.querySelector("#createAccount");
+var passwordContainer = document.querySelector("#passwordContainer");
 
+createAccount.addEventListener("click", (e) => {
+  if (e.target.checked) {
+    passwordContainer.classList.add("show_container");
+  } else {
+    passwordContainer.classList.remove("show_container");
+  }
+});
+
+// SHIPPING ADDRESS
+
+var billingAddress = document.querySelector("#billingAddress");
+var shippingAddressContainer = document.querySelector(
+  "#shippingAddressContainer"
+);
+
+billingAddress.addEventListener("click", (e) => {
+  if (e.target.checked) {
+    shippingAddressContainer.classList.add("hide_container");
+  } else {
+    shippingAddressContainer.classList.remove("hide_container");
+  }
+});
